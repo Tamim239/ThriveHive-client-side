b1c768e65e4e9c7185bc96005ecaeecd844a896e
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-05-10 12:39:56 +0600

    added footer and erorpage handle

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../Layout/MainLayout";
 import { Home } from "../Pages/Home/Home";
+import { ErrorPage } from "../Components/ErrorPage/ErrorPage";
+import { Login } from "../Pages/Login/Login";
 
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
-    //   errorElement: <ErrorPage />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "/",
           element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
       ],
     },
